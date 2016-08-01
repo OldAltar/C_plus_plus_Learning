06b271f9cce0ebe8b0a085e44fd5f7784cd164bb
Old [A]ltar <wh14151017@buaa.edu.cn>
2016-08-01 14:05:18 +0800

    Add files via upload
    My first cpp file.

---
+++ Exercise1_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+/*
+    Computing A+B, both A and B are integers.
+*/
+int main()
+{
+    int v1=0, v2=0;
+    std::cin >>v1>>v2;
+    cout <<v1+v2<< std::endl;
+    return 0;
+}
